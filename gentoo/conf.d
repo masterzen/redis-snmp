ebbb09326797375232ad125564eb2cd74d453c33
Douglas Danger Manley <doug@sevone.com>
2013-04-18 14:30:00 +0000

    Gentoo support: added init.d and conf.d files.
    These will need to be deployed to:
       /etc/init.d/redis-snmp
       /etc/conf.d/redis-snmp

---
+++ gentoo/conf.d
@@ -0,0 +1,29 @@
+# Redis SNMP AgentX configuration
+
+# "host" is the Redis server to connect to.
+# This defaults to:
+#    host = localhost;
+#host=localhost;
+
+# "port" is the port to use to connect to the Redis server.
+# This defaults to:
+#    port = 6379;
+#port=6379;
+
+# "password" is the password for the Redis server.
+# This defaults to:
+#    password =;
+#password=;
+
+# Set "verbose" to "yes" if you would like verbose logging to syslog.
+# Otherwise, leave this as it is.
+# This defaults to:
+#    verbose = no;
+#verbose=no;
+
+# Set "refresh" to how often you would like the information to refresh.
+# This setting is measured in seconds.
+# This defaults to:
+#    refresh = 60;
+#refresh=60;
+
